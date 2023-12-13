d326cb94cf20ef264df55ae8bac778d999697dbb
Mohamed-Mohamed-Hozien <s-mohamed.hozien@zewailcity.edu.eg>
2023-12-13 11:56:54 +0200

    Add project files.

---
+++ Final Project/Models/DB.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Data;
+using System.Data.SqlClient;
+namespace Final_Project.Models
+{
+      
+    public class DB
+    {
+        public SqlConnection con {  get; set; }
+        public DB() {
+            string conStr = "Data Source=Eng_Ziad;Initial Catalog=project;Integrated Security=True";
+            con = new SqlConnection(conStr);       
+        
+        }
+        public DataTable Read(string tableName) { 
+        
+        DataTable dt = new DataTable();
+
+            string Query = "select ID from "+tableName;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Query, con);
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch (SqlException ex)
+            {
+
+            }
+            finally { con.Close();
+            }
+            return dt;
+        }
+    }
+}
---
+++ Final Project/Pages/Admin Screen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final_Project.Pages
+{
+    public class Admin_ScreenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
