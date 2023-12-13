ce8720c85997baf91049716b663240f04710e357
Ziadm90 <zyadalsawy90@gmail.com>
2023-12-13 21:03:08 +0200

    a

--- Final Project/Pages/Shared/Log in Screen.cshtml.cs
+++ Final Project/Pages/Shared/Log in Screen.cshtml.cs
@@ -8,31 +8,36 @@ using System.Data;
 
 namespace Final_Project.Pages.Shared
 {
-   
+
     public class Log_in_ScreenModel : PageModel
     {
         private readonly ILogger<Log_in_ScreenModel> _logger;
         private readonly DB db;
-        public Log_in_ScreenModel(ILogger<Log_in_ScreenModel> logger,DB db)
+        public Log_in_ScreenModel(ILogger<Log_in_ScreenModel> logger, DB db)
         {
             _logger = logger;
             this.db = db;
         }
         [BindProperty]
-        public string idInput { get;set; }
+        public string idInput { get; set; }
         [BindProperty]
         public string passInput { get; set; }
-
+        [BindProperty]
         public DataTable dt { get; set; }
         public void OnGet()
         {
-
         }
         public void OnPost()
         {
-            dt = db.Read("Employee");
-            
-            
+            string Q;
+            Q = "select Role from Employee where E_ID =" + idInput + " and Password= '" + passInput + "'";
+            Console.WriteLine(Q);
+            dt = db.Read("Employee", Q);
+
+            Console.WriteLine();
+
+
+
         }
     }
 }
