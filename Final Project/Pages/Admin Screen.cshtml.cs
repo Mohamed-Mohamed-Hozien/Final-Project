66faed25349af9284aa652fb54c9153cb3d71e88
Ziadm90 <zyadalsawy90@gmail.com>
2023-12-15 13:45:04 +0200

    j

--- Final Project/Pages/Admin Screen.cshtml.cs
+++ Final Project/Pages/Admin Screen.cshtml.cs
@@ -5,8 +5,11 @@ namespace Final_Project.Pages
 {
     public class Admin_ScreenModel : PageModel
     {
+        [BindProperty]
+        public string Case {  get; set; }
         public void OnGet()
         {
+
         }
     }
 }
